3eaca407edb03b5f4e6c38d913891248847a54c1
Helena.Han <hlyhelena37@gmail.com>
2023-12-06 22:35:44 +1100

    bug fix

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -105,7 +105,7 @@ export default function Login() {
 								id="terms"
 								aria-describedby="terms"
 								type="checkbox"
-								class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+								className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
 								required=""
 							/>
 						</div>
