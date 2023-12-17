8bcf315e97cf2c529318631cb8437a79ec00a9c8
Johabela <johabbela@gmail.com>
2023-12-17 22:38:53 +1100

    feat: some code progress in displaying artwork details on the artworks/:id route

--- src/functions/artworkCard.js
+++ src/functions/artworkCard.js
@@ -1,30 +1,57 @@
-async function artworkCard(user, artworkImg, title, description, genre, medium ){ 
-
+async function artworkCardByIdFunc(id, user, artworkImg, title, description, genre, medium) {
+  try {
     let result = await fetch(
-      process.env.REACT_APP_BACKEND_URL + "/artworks/:id/",
+      process.env.REACT_APP_BACKEND_URL + "/artworks/:id/",  
       {
-        method: "GET", 
+        method: "GET",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({
-            user: user, 
-            artworkImg: artworkImg, 
-            title: title, 
-            description: description,
-            genre: genre, 
-            medium: medium,
-
-        }),
+       
       }
     );
 
-	let data = await result.json();
-
+    let data = await result.json();
     console.log(data);
-
-	return data;
-
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error; 
+  }
 }
 
-module.exports = {artworkCard}
+module.exports = { artworkCardByIdFunc };
+
+
+
+// ____________INITIAL FUNCTION ______________________
+// async function artworkCard(user, artworkImg, title, description, genre, medium ){ 
+
+//   let result = await fetch(
+//     process.env.REACT_APP_BACKEND_URL + "/artworks/:id/",
+//     {
+//       method: "GET", 
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({
+//           user: user, 
+//           artworkImg: artworkImg, 
+//           title: title, 
+//           description: description,
+//           genre: genre, 
+//           medium: medium,
+
+//       }),
+//     }
+//   );
+
+// let data = await result.json();
+
+//   console.log(data);
+
+// return data;
+
+// }
+
+// module.exports = {artworkCard}
--- src/pages/ArtworkDetailPage.js
+++ src/pages/ArtworkDetailPage.js
@@ -1,6 +1,7 @@
+// CODE IMPORTING THEE HOOK useArtworkApp  _________________
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { artworks } from '../components/ArtworksCard';
+import ArtworksCard, {useArtworkApp} from '../components/ArtworksCard'; 
 import {
 	HeartIcon,
 	ChatBubbleOvalLeftEllipsisIcon,
@@ -17,13 +18,15 @@ export default function ArtworkDetailPage() {
 	const { showPopup, openPopup, closePopup } = usePopup();
 	const { id } = useParams();
 
-	// Find the selected artwork
-	const selectedArtwork = artworks.find((artwork) => artwork.id === parseInt(id));
-	const selectedArtworkComments = comments.filter((comment) => comment.artworkId === parseInt(id));
 
-	if (!selectedArtwork) {
-		return <div>Artwork not found!</div>;
-	}
+
+  // Use the useArtworkApp hook to get artwork details
+  const selectedArtwork = useArtworkApp(id);
+  const selectedArtworkComments = comments.filter((comment) => comment.artworkId === parseInt(id));
+
+  if (!selectedArtwork) {
+    return <div>Loading...</div>; // You might want to add a loading state here
+  }
 
 	return (
 		<>
@@ -33,9 +36,10 @@ export default function ArtworkDetailPage() {
 				{/* description div */}
 
 				<div className="flex flex-col relative w-full sm:w-2/5">
+				  <ArtworksCard artworkId={id} /> {/* Include ArtworksCard here */}
 					<div className="relative">
 						<img
-							src={selectedArtwork.imageSrc}
+							src={selectedArtwork.artworkImg}
 							alt={selectedArtwork.imageAlt}
 							className="h-3/4 object-cover rounded-lg"
 						/>
@@ -63,7 +67,7 @@ export default function ArtworkDetailPage() {
 					<div className="border border-gray-300 rounded-lg mt-3">
 						<div className="px-4 py-3">
 							<h3 className="text-lg font-semibold">{selectedArtwork.title}</h3>
-							<p className="text-sm text-gray-500">{selectedArtwork.authorname}</p>
+							<p className="text-sm text-gray-500">{selectedArtwork.user}</p>
 						</div>
 					</div>
 
@@ -93,3 +97,104 @@ export default function ArtworkDetailPage() {
 		</>
 	);
 }
+
+
+
+
+
+// ____________ORIGINAL CODE ONLY FRONT END SKELETON ______________
+// import React from 'react';
+// import { useParams } from 'react-router-dom';
+// import { artworks } from '../components/ArtworksCard';
+// import {
+// 	HeartIcon,
+// 	ChatBubbleOvalLeftEllipsisIcon,
+// 	ExclamationTriangleIcon,
+// 	XMarkIcon,
+// } from '@heroicons/react/24/outline';
+// import Header from '../components/Header';
+// import Footer from '../components/Footer';
+// import { usePopup } from '../hooks/usePopup';
+// import CommentPopup from '../components/CommentPopup';
+// import CommentCard, { comments } from '../components/CommentCard';
+
+// export default function ArtworkDetailPage() {
+// 	const { showPopup, openPopup, closePopup } = usePopup();
+// 	const { id } = useParams();
+
+// 	// Find the selected artwork
+// 	const selectedArtwork = artworks.find((artwork) => artwork.id === parseInt(id));
+// 	const selectedArtworkComments = comments.filter((comment) => comment.artworkId === parseInt(id));
+
+// 	if (!selectedArtwork) {
+// 		return <div>Artwork not found!</div>;
+// 	}
+
+// 	return (
+// 		<>
+// 			{' '}
+// 			<Header />
+// 			<div className="bg-white mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 flex flex-col sm:flex-row justify-between items-center">
+// 				{/* description div */}
+
+// 				<div className="flex flex-col relative w-full sm:w-2/5">
+// 					<div className="relative">
+// 						<img
+// 							src={selectedArtwork.imageSrc}
+// 							alt={selectedArtwork.imageAlt}
+// 							className="h-3/4 object-cover rounded-lg"
+// 						/>
+
+// 						{/* Exclamation Icon */}
+// 						<button className="absolute top-0 right-0 mt-2 mr-10">
+// 							<XMarkIcon className="w-6 h-6 text-gray-500" />
+// 						</button>
+// 						<button className="absolute top-0 right-0 mt-2 mr-2">
+// 							<ExclamationTriangleIcon className="w-6 h-6 text-gray-500" />
+// 						</button>
+
+// 						{/* Heart Icon */}
+// 						<button className="absolute bottom-0 right-0 mb-2 mr-12 flex items-center space-x-1 text-gray-600 hover:text-pink-600 focus:outline-none">
+// 							<HeartIcon className="w-6 h-6" />
+// 						</button>
+// 						{/* Comment Icon */}
+// 						<button
+// 							onClick={openPopup}
+// 							className="absolute bottom-0 right-0 mb-2 mr-2 flex items-center space-x-1 text-gray-600 hover:text-indigo-600 focus:outline-none">
+// 							<ChatBubbleOvalLeftEllipsisIcon className="w-6 h-6" />
+// 						</button>
+// 						{showPopup && <CommentPopup onClose={closePopup} />}
+// 					</div>
+// 					<div className="border border-gray-300 rounded-lg mt-3">
+// 						<div className="px-4 py-3">
+// 							<h3 className="text-lg font-semibold">{selectedArtwork.title}</h3>
+// 							<p className="text-sm text-gray-500">{selectedArtwork.authorname}</p>
+// 						</div>
+// 					</div>
+
+// 					<div className="border border-gray-300 rounded-lg mt-3">
+// 						<div className="px-4 py-3 text-gray-600">{selectedArtwork.description}</div>
+// 					</div>
+// 					<div className="border border-gray-300 rounded-lg mt-3">
+// 						<div className="px-4 py-3 text-gray-600">
+// 							<p>
+// 								<strong>Genre:</strong> {selectedArtwork.genre}
+// 							</p>
+// 							<p>
+// 								<strong>Medium:</strong> {selectedArtwork.medium}
+// 							</p>
+// 						</div>
+// 					</div>
+// 				</div>
+
+// 				{/* comment div */}
+// 				<div className=" w-full sm:w-2/5 p-8 mt-8 sm:mt-0">
+// 					{selectedArtworkComments.map((comment) => (
+// 						<CommentCard comment={comment} />
+// 					))}
+// 				</div>
+// 			</div>
+// 			<Footer />
+// 		</>
+// 	);
+// }
