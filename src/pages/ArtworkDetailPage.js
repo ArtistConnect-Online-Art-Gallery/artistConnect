aa3e9e0f71c57efacab32ffdc1a941d8da97cc19
Helena.Han <hlyhelena37@gmail.com>
2023-12-23 17:39:07 +1100

    update backspace icon

--- src/pages/ArtworkDetailPage.js
+++ src/pages/ArtworkDetailPage.js
@@ -4,6 +4,7 @@ import {
 	ChatBubbleOvalLeftEllipsisIcon,
 	ExclamationTriangleIcon,
 	XMarkIcon,
+	ArrowUturnLeftIcon,
 } from '@heroicons/react/24/outline';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
@@ -12,14 +13,13 @@ import CommentPopup from '../components/CommentPopup';
 import CommentCard from '../components/CommentCard';
 import axios from 'axios';
 import { useState } from 'react';
-import baseURL from '../utils/baseURL';  
-import { useSelector } from 'react-redux'; 
-import { IoHeart, IoHeartOutline } from 'react-icons/io5'; 
-
+import baseURL from '../utils/baseURL';
+import { useSelector } from 'react-redux';
+import { IoHeart, IoHeartOutline } from 'react-icons/io5';
 
 export default function ArtworkDetailPage({ artwork }) {
 	const { showPopup, openPopup, closePopup } = usePopup();
-	const navigate = useNavigate()
+	const navigate = useNavigate();
 	const [selectedArtwork, setSelectedArtwork] = useState('');
 	const { id } = useParams();
 
@@ -41,72 +41,71 @@ export default function ArtworkDetailPage({ artwork }) {
 		}
 
 		fetchArtworkById();
-	}, [id]); 
+	}, [id]);
 
 	const userToken = useSelector((state) => state.users?.userAuth?.userInfo?.token);
-	const [isFavorited, setIsFavorited] = useState(false); 
-	const [isReported, setIsReported] = useState(false); 
-  
+	const [isFavorited, setIsFavorited] = useState(false);
+	const [isReported, setIsReported] = useState(false);
+
 	useEffect(() => {
-	  const storedFavoritedArtworks = JSON.parse(localStorage.getItem('favoritedArtworks')) || {};
-	  setIsFavorited(storedFavoritedArtworks[id] || false);
+		const storedFavoritedArtworks = JSON.parse(localStorage.getItem('favoritedArtworks')) || {};
+		setIsFavorited(storedFavoritedArtworks[id] || false);
 	}, [id]);
-  
+
 	const handleFavoriteClick = async () => {
-	  const config = {
-		headers: {
-		  Authorization: `Bearer ${userToken}`,
-		  'Content-Type': 'multipart/form-data',
-		},
-	  };
-  
-	  try {
-		if (isFavorited) {
-		  await axios.delete(`${baseURL}/artworks/${id}/favorite`, config);
-		} else {
-		  await axios.post(`${baseURL}/artworks/${id}/favorite`, {}, config);
-		}
-  
-		const updatedFavoritedArtworks = {
-		  ...(JSON.parse(localStorage.getItem('favoritedArtworks')) || {}),
-		  [id]: !isFavorited,
+		const config = {
+			headers: {
+				Authorization: `Bearer ${userToken}`,
+				'Content-Type': 'multipart/form-data',
+			},
 		};
-		localStorage.setItem('favoritedArtworks', JSON.stringify(updatedFavoritedArtworks));
-  
-		setIsFavorited(!isFavorited);
-	  } catch (error) {
-		console.error(error);
-		console.error('Response:', error.response);
-	  }
+
+		try {
+			if (isFavorited) {
+				await axios.delete(`${baseURL}/artworks/${id}/favorite`, config);
+			} else {
+				await axios.post(`${baseURL}/artworks/${id}/favorite`, {}, config);
+			}
+
+			const updatedFavoritedArtworks = {
+				...(JSON.parse(localStorage.getItem('favoritedArtworks')) || {}),
+				[id]: !isFavorited,
+			};
+			localStorage.setItem('favoritedArtworks', JSON.stringify(updatedFavoritedArtworks));
+
+			setIsFavorited(!isFavorited);
+		} catch (error) {
+			console.error(error);
+			console.error('Response:', error.response);
+		}
 	};
-  
+
 	const handleReportClick = async () => {
 		try {
-		  const config = {
-			headers: {
-			  Authorization: `Bearer ${userToken}`,
-			  'Content-Type': 'application/json',
-			},
-		  };
-	
-		  const payload = {
-			artworkId: id,
-		  };
-	
-		  await axios.post(`${baseURL}/artworks/${id}/report`, payload, config);
-		  setIsReported(true); 
-		} catch (error) {
-		  console.error(error);
-		
-	  };
-	}; 
+			const config = {
+				headers: {
+					Authorization: `Bearer ${userToken}`,
+					'Content-Type': 'application/json',
+				},
+			};
 
-	const handleXClick = function(){
-		navigate(-1)
-	}
+			const payload = {
+				artworkId: id,
+			};
+
+			await axios.post(`${baseURL}/artworks/${id}/report`, payload, config);
+			setIsReported(true);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	const handleXClick = function () {
+		navigate(-1);
+	};
 
 	// distructure comments from selectedArtwork
-	const { comments } = selectedArtwork; 
+	const { comments } = selectedArtwork;
 
 	return (
 		<>
@@ -118,30 +117,28 @@ export default function ArtworkDetailPage({ artwork }) {
 					<div className="relative">
 						<img src={selectedArtwork.artworkImg} alt={''} className="h-3/4 object-cover rounded-lg" />
 						{/* X Icon */}
-						<button className="absolute top-0 right-0 mt-2 mr-10" 
-							onClick={handleXClick}>
-							<XMarkIcon className="w-6 h-6 text-gray-500" />
-						</button> 
+						<button className="absolute top-0 right-0 mt-2 mr-10" onClick={handleXClick}>
+							<ArrowUturnLeftIcon className="w-6 h-6 text-gray-500" />
+						</button>
 						{/* Exclamation Icon */}
-						<button className="absolute top-0 right-0 mt-2 mr-2" 
-							onClick={handleReportClick}>
-							{isReported ? ( 
-							<ExclamationTriangleIcon className="w-6 h-6 text-yellow-500" />
-            				) : (
-							<ExclamationTriangleIcon className="w-6 h-6 text-gray-500 hover:text-yellow-500" />
-            				)}
+						<button className="absolute top-0 right-0 mt-2 mr-2" onClick={handleReportClick}>
+							{isReported ? (
+								<ExclamationTriangleIcon className="w-6 h-6 text-yellow-500" />
+							) : (
+								<ExclamationTriangleIcon className="w-6 h-6 text-gray-500 hover:text-yellow-500" />
+							)}
 						</button>
 
 						{/* Heart Icon */}
 						<button
-        					className="absolute bottom-0 right-0 mb-2 mr-12 flex items-center space-x-1 text-gray-600 hover:text-pink-600 focus:outline-none"
-        					onClick={handleFavoriteClick}>
-        					{isFavorited ? (
-          					<IoHeart className="w-6 h-6 text-pink-600" />
-        					) : (
-          					<IoHeartOutline className="w-6 h-6 hover:text-pink-600" />
-        					)}
-      					</button>
+							className="absolute bottom-0 right-0 mb-2 mr-12 flex items-center space-x-1 text-gray-600 hover:text-pink-600 focus:outline-none"
+							onClick={handleFavoriteClick}>
+							{isFavorited ? (
+								<IoHeart className="w-6 h-6 text-pink-600" />
+							) : (
+								<IoHeartOutline className="w-6 h-6 hover:text-pink-600" />
+							)}
+						</button>
 
 						{/* Comment Icon */}
 						<button
@@ -155,7 +152,7 @@ export default function ArtworkDetailPage({ artwork }) {
 					</div>
 					<div className="border border-gray-300 rounded-lg mt-3">
 						<div className="px-4 py-3">
-						<h3 className="text-2xl font-semibold">Title: {selectedArtwork.title}</h3>
+							<h3 className="text-2xl font-semibold">Title: {selectedArtwork.title}</h3>
 							<p className="text-sm text-gray-500">
 								<Link
 									className="text-gray-600 font-semibold text-lg  hover:text-indigo-800 transition duration-300 ease-in-out hover:underline"
