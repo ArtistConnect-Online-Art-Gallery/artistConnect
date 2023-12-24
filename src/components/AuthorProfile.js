a7ad97aa6c74f196a0ec2e71912f6ee37e1078cd
Helena.Han <hlyhelena37@gmail.com>
2023-12-24 11:55:56 +1100

    Update AuthorProfile.js

--- src/components/AuthorProfile.js
+++ src/components/AuthorProfile.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAuthorProfileAction } from '../redux/slices/users';
-import UserHeader from '../components/UserHeader';
+import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { CheckBadgeIcon } from '@heroicons/react/24/outline';
 import ArtworksCard from '../components/ArtworksCard';
@@ -22,7 +22,7 @@ const UserProfilePage = () => {
 
 	return (
 		<>
-			<UserHeader />
+			<Header />
 
 			<div className="relative block h-500-px">
 				<div>
